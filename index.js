2f5d731015d30d3e2a58ef9ea440c1e0fe842ebb
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-07-08 10:36:27 -0400

    remove unnecessary stuff

--- index.js
+++ index.js
@@ -3,40 +3,9 @@ const cors = require("cors");
 const JDB = require("jsoneng");
 const app = express();
 const db = new JDB("./jdb");
-const http = require("http");
-const server = require("http").createServer(app);
-const WebSocket = require("ws");
 
 app.use(express.json());
 app.use(cors());
-
-const wss = new WebSocket.Server({ server });
-
-app.get("/wait", async (req, res) => {
-  try {
-    let time = 0;
-    async function sendData() {
-      wss.clients.forEach(async (client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          const data = await db.read(req.query.dirname);
-
-          //   console.log(data);
-
-          console.log("we are now sending data to client");
-          time += 1;
-          console.log(time);
-
-          client.send(JSON.stringify(data));
-        }
-      });
-    }
-    // let i = 0;
-    // while (i < 5000000) {
-    setInterval(sendData, 1000);
-    //   i += 1;
-    // }
-  } catch (error) {}
-});
 
 app.post("/create", async (req, res) => {
   try {
@@ -50,13 +19,6 @@ app.post("/create", async (req, res) => {
 app.get("/read", async (req, res) => {
   try {
     const data = await db.read(req.query.dirname);
-
-    // wss.clients.forEach((client) => {
-    //   if (client.readyState === WebSocket.OPEN) {
-    //     client.send("New data created from game js");
-    //   }
-    // });
-
     res.status(200).json(data);
   } catch (error) {
     res.status(500).send(error.message);
@@ -89,8 +51,6 @@ app.delete("/delete", async (req, res) => {
     res.status(500).send(error.message);
   }
 });
-
-server.listen(3002, () => console.log("The websocket on port 3002"));
 
 app.listen(3000, () =>
   console.log("The jsoneng API wrapper is running on port 3000")
