3825c3373070cd037be08717f2096499dbf95843
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-07-08 13:39:22 -0400

    refactor

--- server.py
+++ server.py
@@ -1,57 +1,23 @@
-import os
-import json
 import asyncio
 import websockets
-import copy
 import jsoneng
+import GrandStrategyGame
+
+jdb = jsoneng.JsonDB()
 
 
-# WebSocket server instance
-class Server:
-    def __init__(self):
-        self.json_db = jsoneng.JsonDB()
-        self.connected = set()
-        self.data_cache = {}
+async def echo(websocket, path):
+    async for message in websocket:
+        print(f"Received: {message}")
 
-    async def handler(self, websocket, path):
-        # Register.
-        self.connected.add(websocket)
-        try:
-            # Implement communication with websocket in this loop.
-            # If you want to receive messages from the websocket, you can use: await websocket.recv()
-            # If you want to send messages to the websocket, you can use: await websocket.send("Your message")
-            while True:
-                if websocket.closed:
-                    break
-                await asyncio.sleep(
-                    1
-                )  # Check every second whether the socket is still open.
-
-        finally:
-            # Unregister.
-            self.connected.remove(websocket)
-
-    async def notify_users(self):
-        """_check for changes in JSON data every 5 seconds and notify users."""
-        while True:
-            data = self.json_db.retrieve()
-            if data != self.data_cache:
-                self.data_cache = copy.deepcopy(data)
-                if self.connected:  # whenever changes are detected
-                    message = json.dumps(self.data_cache)
-                    await asyncio.wait([user.send(message) for user in self.connected])
-
-            print("bye")
-            await asyncio.sleep(5)  # sleep for 5 seconds
-
-    def run(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(websockets.serve(self.handler, "localhost", 8765))
-        loop.run_until_complete(self.notify_users())
-        loop.run_forever()
+        if message:
+            GrandStrategyGame.gen()
+            out = jdb.retrieve("test0")
+        print(out)
+        await websocket.send(str(out))
 
 
-# Run the server
-if __name__ == "__main__":
-    server = Server()
-    server.run()
+start_server = websockets.serve(echo, "localhost", 9000)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
